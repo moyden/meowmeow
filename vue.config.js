410c3d5f9eee67f9488fc6bb58f739f8c50714fc
Joseph Moyden <josephmoyden@Josephs-MacBook-Pro.local>
2020-07-04 01:15:42 +0100

    PWA setup and favicons

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,12 @@ module.exports = {
         prependData: `@import "~@/variables.scss";`
       }
     }
+  },
+  pwa: {
+    name: 'Meow Meow',
+    themeColor: 'rgb(255, 210, 48)',
+    msTileColor: 'rgb(255, 210, 48)',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'default'
   }
 }
